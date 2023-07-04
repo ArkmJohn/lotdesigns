648ab0fcf75d1e3091b21fe9c78c1cd62a9c4a66
John Mark Adriano <adrianojohnmark@gmail.com>
2023-07-04 22:54:04 +0800

    logo fix

--- src/app/Components/header.js
+++ src/app/Components/header.js
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import logo from '../Images/icon.png';
 
 const headerStyle = {
   minHeight: '7vh',
@@ -9,16 +8,21 @@ const headerStyle = {
   zIndex: 9,
 }
 
+const logo = {
+  backgroundImage: "url('/Images/icon.png')",
+    width: '100%',
+    height: 'auto',
+    backgroundSize: 'contain',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    margin: '5px',
+}
+
 export default function Header() {
     return (
       <nav  style={headerStyle} className="min-w-full flex justify-between border-gray-200 border-solid border-b-2">
         <div className='relative flex jusify-between h-20 w-40 border-gray-200 border-solid border-r-2'>
-          <Image
-            src={logo}
-            alt="Lot Designs"
-            className='object-contain py-2'
-            fill
-          />
+          <div style={logo} className='object-contain py-2'>  </div>
         </div>
         <ul className='hidden min-h-full flex items-center uppercase text-sm font-bold pr-8'>
           <li className='px-5'><a href="#">Home</a></li>
